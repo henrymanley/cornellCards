c4672506354bc625b0942013084e9b660b3c1f1b
Reza Madhavan <rm855@cornell.edu>
2021-03-07 14:55:15 -0500

    Backend shit

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
 import {useParams} from 'react-router-dom'
+import {useHistory} from 'react-router'
 import firebase from './firebase'
+import { SignalCellularNull } from '@material-ui/icons';
 
 export default function GamePage() {
     const createId = () => {
@@ -14,45 +16,67 @@ export default function GamePage() {
       return pin
     }
 
-    const createGame = async (gameId) => {
-      const gameDocRef = firebase.firestore().collection('gameSessions').doc()
-      const hostParticipantRef = gameDocRef.collection('participants').doc()
-      await gameDocRef.set({'host': {'name': 'Reza', 'id': hostParticipantRef.id}, 'participants': {}, 'pin': gameId})
-      await hostParticipantRef.set({'cards': {}, 'name': 'Reza', 'score': 0})
-    }
+    const [gameId, setGameId] = useState(useParams()['gameId'])
+    const [gameDocTest, setGameDocTest] = useState(null)
+    const [error, setError] = useState(false)
+    const [host, setHost] = useState(false)
+    const history = useHistory()
 
-    const joinGame = async (gameId) => {
-      const gameDoc = await firebase.firestore().collection('gameSessions').where('pin', '==', gameId).get()
-      console.log(gameDoc)
-      console.log(gameDoc)
-      if (gameDoc.empty){
-        console.log('invalid game pin')
+    const [data, setData] = useState({host: false, gameId: useParams()['gameId'], gameDocTest: null })
+
+    useEffect(async () => {
+
+      if (data.gameId) {
+        const gameDocQuery = await firebase.firestore().collection('gameSessions').where('pin', '==', gameId).get()
+        console.log(gameDocQuery)
+        if (gameDocQuery.empty){
+          setError(true)
+          console.log('invalid game pin')
+        }
+        
+        else{
+          const gameDoc = gameDocQuery.docs[0]
+          const newParticipantDocRef = gameDoc.ref.collection('participants').doc()
+          const newParticipantPromise = newParticipantDocRef.set({'cards': {}, 'name': 'new Participant promise', 'score': 0})
+          let gameDocUpdatePromise;
+          if (!gameDoc.data().host){
+            gameDocUpdatePromise = gameDoc.ref.update({'host': {'name': 'Reza No Host', 'id': newParticipantDocRef.id}, 'participants': firebase.firestore.FieldValue.arrayUnion({'id': newParticipantDocRef.id, 'name': 'Reza No Host'})})
+          }
+          else{
+            gameDocUpdatePromise = gameDoc.ref.update({'participants': firebase.firestore.FieldValue.arrayUnion({'id': newParticipantDocRef.id, 'name': 'Reza Host Participant'})})
+          }
+          await Promise.all([newParticipantPromise, gameDocUpdatePromise])
+          console.log('Joined game!')
+        }
       }
       else{
-        const newParticipantDocRef = gameDoc.collection('participants').doc()
-        await newParticipantDocRef.set({'cards': {}, 'name': 'Henry', 'score': 0})
-        await gameDoc.update({'participants': firebase.firestore.FieldValue.arrayUnion({'id': newParticipantDocRef.id, 'name': 'Henry'})})
-        console.log('Joined game! Host: ' + gameDoc.data().host.name)
+        const gameDocRef = firebase.firestore().collection('gameSessions').doc()
+        const newId = createId()
+        await gameDocRef.set({'participants': {}, 'pin': newId, 'gameStarted': false, 'round': 0})
+        history.replace('/game/' + newId)
+        setData({host: true, gameId: newId, gameDocTest: gameDocRef })
+        // setHost(true)
+        // setGameId(newId)
+        
       }
-    }
-
-
-    var {gameId} = useParams()
-    if (!gameId){
-      gameId = createId()
-      createGame(gameId)
-    }
-    else{
-      joinGame(gameId)
-    }
+    }, [gameId])
 
     
+    useEffect(() => {
+      return async () => {
+        console.log("unmounting")
+        await data.gameDocRef.delete();
+      }
+    }, [])
 
-    
 
     return (
+        data.host ? 
         <div>
-            Game id: {gameId}
+            You are the host! Game id: {data.gameId}
+        </div> :
+        <div>
+          You joined the game! Game id: {data.gameId}
         </div>
     );
 }
